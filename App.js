fcabaa79f5eefb805536cfb1c85c4a1d9e366dd7
peopleisme <socko.piotr@o2.pl>
2022-09-25 22:28:06 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type { Node } from 'react';
+import {
+  Component,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import Canvas from 'react-native-canvas';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+
+
+const App: () => Node = () => {
+
+
+  return (
+    <View >
+      
+      
+    </View>
+  )
+};  
+
+
+export default App;
